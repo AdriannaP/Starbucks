546d37b8e2fc349175a30de35b6e03da1b998e5a
Ada Paczocha <ada-paczocha@o2.pl>
2020-09-28 17:32:09 +0200

    CoffeeModel

---
+++ Starbucks/Models/Coffee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Starbucks.Models
+{
+    public class Coffee
+    {
+        string NameCoffee { get; set; }
+        int Price { get; set; }
+        string Description { get; set; }
+        int Id { get; set; }
+
+    }
+}
