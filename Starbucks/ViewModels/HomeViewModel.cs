cb2b830a1b8421a08dafafe33f8cc6fc777d946f
Ada Paczocha <ada-paczocha@o2.pl>
2020-10-01 00:33:05 +0200

    First View

---
+++ Starbucks/ViewModels/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Starbucks.ViewModels
+{
+    public class HomeViewModel
+    {
+    }
+}
