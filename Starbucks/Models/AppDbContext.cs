84ffb855b96734c3bcbbda90e6a2472990e42a1b
Ada Paczocha <ada-paczocha@o2.pl>
2020-11-17 17:52:41 +0100

    implementing user creation and authorization

--- Starbucks/Models/AppDbContext.cs
+++ Starbucks/Models/AppDbContext.cs
@@ -1,14 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Starbucks.Areas.Identity.Data;
 using Starbucks.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Starbucks
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<IdentityUser>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         { }
